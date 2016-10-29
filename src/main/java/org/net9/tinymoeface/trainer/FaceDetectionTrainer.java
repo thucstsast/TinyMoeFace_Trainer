99c69a34e2711baa3b534a3f1e3ac2542f4424bd
Tan Si Nan <tansinan_1995@163.com>
2016-10-29 23:00:01 +0800

    Initialize an empty java project with gradle build system.

---
+++ src/main/java/org/net9/tinymoeface/trainer/FaceDetectionTrainer.java
@@ -0,0 +1,9 @@
+package org.net9.tinymoeface.trainer;
+
+public class FaceDetectionTrainer
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a stub.");
+    }
+}
